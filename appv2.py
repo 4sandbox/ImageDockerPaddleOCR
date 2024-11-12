37e4c0f0823fd1e16796e23ac42775232879fa2a
4sandbox <jhana3455@gmail.com>
2024-11-12 17:30:16 +0000

    add models

--- appv2.py
+++ appv2.py
@@ -2,8 +2,15 @@ import json
 import argparse
 from paddleocr import PaddleOCR
 
-def ocr_to_json(img_path, lang='vi', font_path=None):
-    ocr = PaddleOCR(use_angle_cls=True, lang=lang)
+def ocr_to_json(img_path, lang='vi', det_model_dir=None, rec_model_dir=None, cls_model_dir=None):
+    # Khởi tạo PaddleOCR với các model directory được chỉ định
+    ocr = PaddleOCR(
+        use_angle_cls=True,
+        lang=lang,
+        det_model_dir=det_model_dir,
+        rec_model_dir=rec_model_dir,
+        cls_model_dir=cls_model_dir
+    )
     result = ocr.ocr(img_path, cls=True)
 
     json_result = []
@@ -22,8 +29,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Perform OCR on an image file.")
     parser.add_argument("img_path", help="Path to the image file")
     parser.add_argument("--lang", default='vi', help="Language for OCR")
-    parser.add_argument("--font_path", help="Path to the font file")
+    parser.add_argument("--det_model_dir", default="models/en_PP-OCRv3_det_infer", help="Path to the detection model directory")
+    parser.add_argument("--rec_model_dir", default="models/latin_PP-OCRv3_rec_infer", help="Path to the recognition model directory")
+    parser.add_argument("--cls_model_dir", default="models/ch_ppocr_mobile_v2.0_cls_infer", help="Path to the classification model directory")
 
     args = parser.parse_args()
-    result = ocr_to_json(args.img_path, lang=args.lang, font_path=args.font_path)
+    result = ocr_to_json(
+        args.img_path,
+        lang=args.lang,
+        det_model_dir=args.det_model_dir,
+        rec_model_dir=args.rec_model_dir,
+        cls_model_dir=args.cls_model_dir
+    )
     print(result)
